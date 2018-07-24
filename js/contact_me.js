ef942a2c70ad2612e07d054b7f4987bc77a9a6f2
rafyluc <r.dellaporta5@studenti.unisa.it>
2018-07-24 13:55:47 +0200

    sito prova

--- js/contact_me.js
+++ js/contact_me.js
@@ -22,7 +22,7 @@ $(function() {
 		
 	
             $.ajax({
-                url: "https://github.com/rafyluc/prova.github.io/blob/master/js/invioDati.php",
+                url: "https://localhost//invioDati.php",
                 type: "POST",
                 data: {
                     name: name,
